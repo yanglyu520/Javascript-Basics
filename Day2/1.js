f4b0b663dcac456d62ff3afda1d9440f6691c848
Yang Lyu <yang.lyu@hotmail.com>
2023-01-16 05:49:52 -0500

    day 2 prac

---
+++ Day2/1.js
@@ -0,0 +1,7 @@
+//special 3 elements expression
+3 > 5 ? console.log('true') : console.log('false')
+//find the larger number of the two numbers
+let num1 = 4
+let num2 = 5
+let var1 = num1 > num2 ? num1 : num2
+console.log(var1)
