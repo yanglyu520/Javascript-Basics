ba705ca290b4f3fd835a8e4ba568c84b78c764f2
Yang Lyu <yang.lyu@hotmail.com>
2023-01-24 01:36:38 -0500

    update

--- Day5/object.js
+++ Day5/object.js
@@ -8,7 +8,21 @@ let person = {
   }
 }
 
+//add or edit a field
+person.hobby = 'football'
+person.sex = 'male'
+
+//delete a field
+delete person.name
+
 console.log(person)
 console.log(person.age)
 console.log(person['sex'])
 person.sayHi()
+
+//go through all elements in an object
+for (let k in person) {
+  console.log(k)
+
+  console.log(person[k])
+}
